183e1446823ba37681a08df65c47c6fa21c0c4c4
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-11 22:16:52 +0800

    adjust height and width

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import ReactPlayer from "react-player";
 import video from "./assets/marsha-tandingan.mp4";
 import { Flex } from "@chakra-ui/react";
+import "./app.css";
 
 function App() {
 
   return (
-    <Flex height="100vh">
-  
-      <ReactPlayer url={video} controls="true" width="100%"/>
-    </Flex>
+        <ReactPlayer url={video} controls={true} height="100vh" width="100%" />
   )
 }
 
