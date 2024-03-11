a30995470a7ae56fce09164b1d92bdf7c3834e00
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-11 22:10:28 +0800

    added height and width

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import ReactPlayer from "react-player";
 import video from "./assets/marsha-tandingan.mp4";
+import { Flex } from "@chakra-ui/react";
 
 function App() {
 
   return (
-    <>
+    <Flex height="100vh">
   
-      <ReactPlayer url={video} controls="true" />
-    </>
+      <ReactPlayer url={video} controls="true" width="100%"/>
+    </Flex>
   )
 }
 
